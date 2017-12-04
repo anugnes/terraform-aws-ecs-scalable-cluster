e51770cdb7479b10c850e1139a9a08494fed75df
anugnes <nugnes.alberto@gmail.com>
2017-12-04 15:21:08 +0000

    Commit various changes to the repo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 // fetch an ECS optimised Amazon AMI in the selected region
-data "aws_ami" "amazon_ecs_os" {
+data "aws_ami" "amazon_ecs_ami" {
   most_recent = true
 
   filter {
@@ -47,12 +47,38 @@ resource "aws_iam_instance_profile" "cluster" {
   role = "${aws_iam_role.ecs_cluster.name}"
 }
 
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs_sg"
+  vpc_id      = "${var.vpc_id}"
+  description = "ECS nodes default security group"
+
+  tags {
+    Name = "${var.cluster_name}"
+  }
+
+  ingress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_launch_configuration" "ecs_conf" {
-  name                 = "${var.cluster_name}-LC"
-  image_id             = "${data.aws_ami.amazon_ecs_os.id}"
-  iam_instance_profile = "${aws_iam_instance_profile.cluster.name}"
-  instance_type        = "${var.type}"
-  key_name             = "${var.key_name}"
+  name                        = "${var.cluster_name}-LC"
+  image_id                    = "${data.aws_ami.amazon_ecs_ami.id}"
+  iam_instance_profile        = "${aws_iam_instance_profile.cluster.name}"
+  instance_type               = "${var.type}"
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = false
+  security_groups             = ["${aws_security_group.ecs_sg.id}"]
 
   lifecycle {
     create_before_destroy = true
@@ -61,10 +87,95 @@ resource "aws_launch_configuration" "ecs_conf" {
   user_data = "${data.template_file.user_data.rendered}"
 }
 
+resource "aws_autoscaling_policy" "scale_out" {
+  adjustment_type = "ChangeInCapacity"
+  policy_type     = "StepScaling"
+
+  step_adjustment {
+    scaling_adjustment          = 1
+    metric_interval_lower_bound = "0"
+    metric_interval_upper_bound = ""
+  }
+
+  autoscaling_group_name = "${aws_autoscaling_group.ecs.name}"
+  name                   = "simple_scale_out"
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_greater_than" {
+  alarm_name          = "cpu_usage_greater_than"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.ecs.name}"
+  }
+
+  alarm_description = "This metric monitors EC2 CPU utilisation greater than 80%"
+  alarm_actions     = ["${aws_autoscaling_policy.scale_out.arn}"]
+}
+
+resource "aws_autoscaling_policy" "scale_in" {
+  adjustment_type = "ChangeInCapacity"
+  policy_type     = "StepScaling"
+
+  step_adjustment {
+    scaling_adjustment          = "-1"
+    metric_interval_lower_bound = ""
+    metric_interval_upper_bound = "0"
+  }
+
+  autoscaling_group_name = "${aws_autoscaling_group.ecs.name}"
+  name                   = "simple_scale_in"
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_less_than" {
+  alarm_name          = "cpu_usage_less_than"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 20
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.ecs.name}"
+  }
+
+  alarm_description = "This metric monitors EC2 CPU utilisation less than 20%"
+  alarm_actions     = ["${aws_autoscaling_policy.scale_in.arn}"]
+}
+
 resource "aws_autoscaling_group" "ecs" {
-  name                 = "${var.cluster_name}-ASG"
-  launch_configuration = "${aws_launch_configuration.ecs_conf.name}"
-  vpc_zone_identifier  = ["${split(",", var.subnet_id)}"]
-  max_size             = "${var.cluster_max_size}"
-  min_size             = "${var.cluster_min_size}"
+  name                      = "${var.cluster_name}-ASG"
+  launch_configuration      = "${aws_launch_configuration.ecs_conf.name}"
+  vpc_zone_identifier       = ["${split(",", var.subnet_id)}"]
+  max_size                  = "${var.cluster_max_size}"
+  min_size                  = "${var.cluster_min_size}"
+  default_cooldown          = 60
+  health_check_type         = "EC2"
+  health_check_grace_period = "10"
+  termination_policies      = ["OldestInstance"]
+
+  enabled_metrics = [
+    "GroupMaxSize",
+    "GroupMinSize",
+    "GroupInServiceInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTotalInstances",
+  ]
+
+  tag {
+    key                 = "Name"
+    propagate_at_launch = true
+    value               = "ecs-node"
+  }
 }
